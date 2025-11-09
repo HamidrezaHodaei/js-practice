090fbf66d8ed43d8f00388c64f3ab9f00e1a5205
mauve2412 <hodaeiihamidreza@gmail.com>
2025-11-10 02:27:19 +0330

    avg.js

---
+++ avg.js
@@ -0,0 +1,11 @@
+function avgarr(arr){
+    let sum =0;
+    for(let i=0 ; i<arr.length;i++){
+        sum += arr[i]
+    }
+    let avg = sum/arr.length
+    return avg
+}
+
+
+console.log(avgarr([1, 2, 3, 4, 5])); 
